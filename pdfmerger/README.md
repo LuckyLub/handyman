c44647dacfbe6c27557cb05ec163c0eff3cfcea7
Your Name <rjlub@outlook.com>
2020-05-01 11:31:38 +0200

    implemented ui for selecting file and folders.

---
+++ pdfmerger/README.md
@@ -0,0 +1,8 @@
+# PDFMerger
+Merges selected pdfs and images into 1 pdf. Images are converted to A4 file format.
+
+- Create a folder with only pdf's and images which you want to merge. **NOTE:** .heic images are not supported. You'll need to convert them yourself.
+- Run the script.
+- A window will pop-up asking you to select the file you want to have on top of your final merged pdf. This first file can, but does not have to be within the folder with the other images and pdfs.
+- Next a window will pop-up asking you to select the folder with all the pdfs and images that you want to merge.
+- At last a window will pop-up asking you where and with what name you want to store the pdf.
--- pdfmerger/main.py
+++ pdfmerger/main.py
@@ -1,48 +1,88 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename, askdirectory, asksaveasfile
 import img2pdf 
 import os 
+from pathlib import Path
+
 from PIL import Image
 
+def image2Pdf(folder:str, img:str, outputPath:str=None)->str:
+    """ Converts an image to a pdf in A4 format. Does not support HEIC format.
 
-def merger(first_file, folder, output):
+    Arguments:
+        folder: string, path where image is stored.
+        img: string, name of image. Should include file extension.
+        outputPath: string, optional, path to directory where pdf should be
+            stored.
+
+    Returns:
+        Path to resulting pdf as string.
+    """
+    imgName = img[:img.rfind(".")]
+    if outputPath:
+        imgPdf = f"{outputPath}/{imgName}.pdf"
+    else: 
+        imgPdf = f"{imgName}.pdf"
+    imgPath = os.path.join(folder, img)
+    # Delete alpha channel by converting to RGB. Save as temp file.
+    with Image.open(imgPath) as img:
+        img = img.convert('RGB')
+        width, height = img.size
+        img.save('temp.jpeg', format='JPEG')
+    # Determine orientation of the image and set pdf size accordingly
+    if width < height:
+        a4Size = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+    else:
+        a4Size = (img2pdf.mm_to_pt(297),img2pdf.mm_to_pt(210))
+    layout = img2pdf.get_layout_fun(a4Size)
+    # Convert the image to pdf, delete temp file.
+    pdfBytes = img2pdf.convert('temp.jpeg', layout_fun=layout)
+    with open(imgPdf, 'wb') as fout:
+        fout.write(pdfBytes)
+    os.remove('temp.jpeg')
+    return imgPdf
+
+def merger():
+    """ Merges a pdf with all the pdfs and images from the selected folder."""
+    Tk().withdraw()
+    # Select the file you want to get on top.
+    firstFile = askopenfilename(title = "Select the file you want on top.")
+    if not firstFile:
+        return None
+    # Select the directory containing all pdfs and images you want to merge. 
+    folder = askdirectory(title = "Select the folder containing the files you "
+                                  "want to merge.")
+    if not folder:
+        return None
+    
+    deleters = [] # collector for the temp pdfs created for images.
     pdfs = PdfFileMerger()
-    pdfs.append(first_file)
-    deleters = []
-    for file in os.listdir(folder):
-        if file.endswith(".pdf"):
-            pdfs.append(os.path.join(folder, file))
+    
+    files = [os.path.split(Path(firstFile))]
+    for f in os.listdir(folder):
+        files.append((folder, f))
+
+    for index, (p, f) in enumerate(files):
+        # if the first file is in the same folder as the rest of the files
+        # only add it once.
+        if index != 0 and Path(os.path.join(p,f)) == Path(firstFile):
+            continue
+        elif f.endswith(".pdf"):
+            pdfs.append(os.path.join(folder, f))
         else:
-            img_name = file[:file.rfind(".")]
-            img_pdf = "img_name.pdf"
-            deleters.append(img_pdf)
-            if not img_pdf in os.listdir(folder):
-                img_path = os.path.join(folder, file)
-                with Image.open(img_path) as img:
-                    img = img.convert('RGB')
-                    width, height = img.size
-                    img.save('temp.jpeg', format='JPEG')
-                if width < height:
-                    a4size = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
-                else:
-                    a4size = (img2pdf.mm_to_pt(297),img2pdf.mm_to_pt(210))
-                layout_fun  = img2pdf.get_layout_fun(a4size)
-                pdf_bytes = img2pdf.convert('temp.jpeg', layout_fun=layout_fun)
-                with open(img_pdf, 'wb') as fout:
-                    fout.write(pdf_bytes)
-                pdfs.append(img_pdf)
-                os.remove('temp.jpeg')
-    pdfs.write(output + ".pdf")
+            pdfImage = image2Pdf(folder, f)
+            pdfs.append(pdfImage)
+            deleters.append(pdfImage)
+    
+    f = asksaveasfile(defaultextension=".pdf")
+    output = f.name
+    f.close()
+    pdfs.write(output)
     pdfs.close()
     for pdf in deleters:
         os.remove(pdf)
+    os.startfile(output)
 
 if __name__ == "__main__":
-    from tkinter import Tk
-    from tkinter.filedialog import askopenfilename, askdirectory
-    Tk().withdraw()
-    # Select the file you want to get on top.
-    firstPdf = askopenfilename() 
-    # Select the directory containing all pdfs and images you want to merge. 
-    dirPdfs = askdirectory()
-    nameOutput = "mijnoutput"
-    merger(firstPdf, dirPdfs, nameOutput)
+    merger()
